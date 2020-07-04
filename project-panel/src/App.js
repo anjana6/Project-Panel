72002edc6818977b9767844f9b0e67bdef512c0f
anjana6 <anjanashakthi95@gmail.com>
2020-07-04 20:44:22 +0530

    create component

---
+++ project-panel/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        app
+      </div>
+    </Router>
+    
+  );
+}
+
+export default App;
