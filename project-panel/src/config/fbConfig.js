75fed052c066ee5cf30623e76a213f98de82d050
anjana6 <anjanashakthi95@gmail.com>
2020-07-13 09:08:47 +0530

    connect firebase and redux

---
+++ project-panel/src/config/fbConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBLDL5olzzW1pmFObxLh06nUuDErRZfw9Q",
+    authDomain: "project-panel-6b126.firebaseapp.com",
+    databaseURL: "https://project-panel-6b126.firebaseio.com",
+    projectId: "project-panel-6b126",
+    storageBucket: "project-panel-6b126.appspot.com",
+    messagingSenderId: "589771175494",
+    appId: "1:589771175494:web:7dc67fe2062fb5492ffc47"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.firestore().settings({timestampsInSnapshots:true});
+
+  export default firebase;
--- project-panel/src/configStore.js
+++ project-panel/src/configStore.js
@@ -1,8 +1,17 @@
-import {createStore,applyMiddleware} from 'redux';
+import {createStore,applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk'
 import rootReducer from './store/reducer';
+import {reduxFirestore,getFirestore} from 'redux-firestore';
+import {reactReduxFirebase,getFirebase} from 'react-redux-firebase';
+import fbConfig from './config/fbConfig';
 
 // const thunk = [thunk]
-const configStore = createStore(rootReducer,applyMiddleware(thunk))
+const configStore = createStore(rootReducer,
+    compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase,getFirestore})),
+    reduxFirestore(fbConfig),
+    reactReduxFirebase(fbConfig),
+)
+    );
 
 export default configStore;
